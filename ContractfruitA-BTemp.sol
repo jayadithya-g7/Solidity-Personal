b908522ca305e004678307dd8838c15611501c72
0xethjayadithya_g7/ <144100792+jayadithya-g7@users.noreply.github.com>
2024-07-18 11:32:11 +0530

    Update ContractfruitA-BTemp.sol

--- ContractfruitA-BTemp.sol
+++ ContractfruitA-BTemp.sol
@@ -1,4 +1,4 @@
-A&B entering a contract where A->B There's transfer of fruits and B->A, we need transfer of money based on the check that, food is not disturbed , maintained at a temp of 30deg, only then food is not disturbed..
+//A&B entering a contract where A->B There's transfer of fruits and B->A, we need transfer of money based on the check that, food is not disturbed , maintained at a temp of 30deg, only then food is not disturbed..
 ---------------------------------------------------------------------------------------
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
