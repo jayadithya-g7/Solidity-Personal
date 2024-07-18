c7a44a7aed462d639c2c42c3bea6cc0cff724e41
0xethjayadithya_g7/ <144100792+jayadithya-g7@users.noreply.github.com>
2024-07-18 11:31:36 +0530

    Create ContractfruitA-BTemp.sol

---
+++ ContractfruitA-BTemp.sol
@@ -0,0 +1,14 @@
+A&B entering a contract where A->B There's transfer of fruits and B->A, we need transfer of money based on the check that, food is not disturbed , maintained at a temp of 30deg, only then food is not disturbed..
+---------------------------------------------------------------------------------------
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract temperature {
+    function checkTemp(uint256 temp) public pure returns (string memory) {
+        if (temp < 30) {
+            return /*Type your stament here in double quotes*/;
+        } else {
+            return /*Type your stament here in double quotes*/;
+        }
+    }
+}
