133504d17b97be0b9896c876b419c769b9cbc5f8
0xethjayadithya_g7/ <144100792+jayadithya-g7@users.noreply.github.com>
2024-08-01 13:26:11 +0530

    Helloworld.sol

---
+++ Helloworld.sol
@@ -0,0 +1,17 @@
+//1st thing is writing the spdx license no.
+// (my first handwritten contract on solidity)
+
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+//give contract name
+
+contract demo{
+
+    string public str="hello world";
+    //there are no print statements in solidity, that's why we use this method to print files
+
+}
+
+//now just compile and deploy
